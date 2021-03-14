e6777abc42e75e50014d7598637fc77e757ba2fb
yurak <yurak@DESKTOP-94P3ACC>
2021-03-14 16:45:00 +0200

    init

---
+++ Receiver/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace Receiver
+{
+    internal class Program
+    {
+        private static string _connectionString;
+        private static string _queueName;
+
+        private static async Task Main()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            _connectionString = config.GetSection("connectionString")?.Value;
+            _queueName = config.GetSection("queueName")?.Value;
+          
+            // receive message from the queue
+            await ReceiveMessagesAsync();
+        }
+
+        private static async Task ReceiveMessagesAsync()
+        {
+            await using var client = new ServiceBusClient(_connectionString);
+            // create a processor that we can use to process the messages
+            var processor = client.CreateProcessor(_queueName, new ServiceBusProcessorOptions());
+
+            // add handler to process messages
+            processor.ProcessMessageAsync += MessageHandler;
+
+            // add handler to process any errors
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            // start processing 
+            await processor.StartProcessingAsync();
+
+            Console.WriteLine("Wait for a minute and then press any key to end the processing");
+            Console.ReadKey();
+
+            // stop processing 
+            Console.WriteLine("\nStopping the receiver...");
+            await processor.StopProcessingAsync();
+            Console.WriteLine("Stopped receiving messages");
+        }
+
+        // handle received messages
+        private static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            var body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            // complete the message. messages is deleted from the queue. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        private static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace Sender
+{
+    internal class Program
+    {
+        private static string _connectionString;
+        private static string _queueName;
+        private static async Task Main()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+            _connectionString = config.GetSection("connectionString")?.Value;
+            _queueName = config.GetSection("queueName")?.Value;
+
+            // send a message to the queue
+            await SendMessageAsync();
+            // Console.WriteLine("Hello World!");
+        }
+
+        private static async Task SendMessageAsync()
+        {
+            // create a Service Bus client 
+            await using (var client = new ServiceBusClient(_connectionString))
+            {
+                // create a sender for the queue 
+                var sender = client.CreateSender(_queueName);
+
+                // create a message that we can send
+                var message = new ServiceBusMessage("Hello world!");
+
+                // send the message
+                await sender.SendMessageAsync(message);
+                Console.WriteLine($"Sent a single message to the queue: {_queueName}");
+            }
+        }
+    }
+}
